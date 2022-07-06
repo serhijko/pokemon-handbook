816155b9a648306e9e45238c642a3e0aadf79ad8
Siarhei Karnialiuk <sp_by@tut.by>
2022-07-06 10:03:15 +0300

    implementation of post, get, update & delete functions to work with MondoDB (including InsertOne, Find, FindOne, FindOneAndUpdate, DeleteOne functions)

--- pokemon.go
+++ pokemon.go
@@ -20,19 +20,15 @@ type pokemon struct {
 	Color       string `json:"color"`
 }
 
-var pokemons = []pokemon{
-	{ID: "1", Name: "bulbasaur", IsLegendary: false, Color: "green"},
-	{ID: "4", Name: "charmander", IsLegendary: false, Color: "red"},
-	{ID: "25", Name: "pikachu", IsLegendary: false, Color: "yellow"},
-	{ID: "54", Name: "psyduck", IsLegendary: false, Color: "yellow"},
-}
-
 func init() {
 	fmt.Println("This is init")
-}
 
-func main() {
-	fmt.Println("This is main")
+	var pokemons = []pokemon{
+		{ID: "1", Name: "bulbasaur", IsLegendary: false, Color: "green"},
+		{ID: "4", Name: "charmander", IsLegendary: false, Color: "red"},
+		{ID: "25", Name: "pikachu", IsLegendary: false, Color: "yellow"},
+		{ID: "54", Name: "psyduck", IsLegendary: false, Color: "yellow"},
+	}
 
 	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
 	defer cancel()
@@ -55,29 +51,10 @@ func main() {
 		id := res.InsertedID
 		fmt.Printf("id value %v\n", id)
 	}
+}
 
-	cur, err := collection.Find(context.Background(), bson.D{})
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer cur.Close(context.Background())
-	for cur.Next(context.Background()) {
-		// To decode into a struct, use cursor.Decode()
-		result := pokemon{}
-		err := cur.Decode(&result)
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Printf("Pokemon entry: %v\n", result)
-
-		// To get the raw bson bytes use cursor.Current
-		raw := cur.Current
-		fmt.Printf("Raw result entry: %v\n", raw)
-	}
-	if err := cur.Err(); err != nil {
-		fmt.Println(err)
-		return
-	}
+func main() {
+	fmt.Println("This is main")
 
 	router := gin.Default()
 	router.POST("/pokemons", postPokemons)
@@ -93,27 +70,98 @@ func postPokemons(c *gin.Context) {
 	var newPokemon pokemon
 
 	if err := c.BindJSON(&newPokemon); err != nil {
+		// c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "object can't be parsed into JSON"})
 		return
 	}
 
-	pokemons = append(pokemons, newPokemon)
+	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://root:example@localhost:27017"))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("Client value %v\n", client)
+
+	collection := client.Database("pokemon-book").Collection("pokemon")
+	fmt.Printf("Collection value %v\n", collection)
+
+	res, err := collection.InsertOne(context.Background(), newPokemon)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	id := res.InsertedID
+	fmt.Printf("id value %v\n", id)
+
+	// pokemons = append(pokemons, newPokemon)
 	c.IndentedJSON(http.StatusCreated, newPokemon)
 }
 
 func getPokemons(c *gin.Context) {
+	var pokemons = []pokemon{}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://root:example@localhost:27017"))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("Client value %v\n", client)
+
+	collection := client.Database("pokemon-book").Collection("pokemon")
+	fmt.Printf("Collection value %v\n", collection)
+
+	cur, err := collection.Find(context.Background(), bson.D{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer cur.Close(context.Background())
+	for cur.Next(context.Background()) {
+		// To decode into a struct, use cursor.Decode()
+		result := pokemon{}
+		err := cur.Decode(&result)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("Pokemon entry: %v\n", result)
+		pokemons = append(pokemons, result)
+
+		// To get the raw bson bytes use cursor.Current
+		raw := cur.Current
+		fmt.Printf("Raw result entry: %v\n", raw)
+	}
+	if err := cur.Err(); err != nil {
+		fmt.Println(err)
+		return
+	}
 	c.IndentedJSON(http.StatusOK, pokemons)
 }
 
 func getPokemonByID(c *gin.Context) {
 	id := c.Param("id")
 
-	for _, a := range pokemons {
-		if a.ID == id {
-			c.IndentedJSON(http.StatusOK, a)
-			return
-		}
+	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://root:example@localhost:27017"))
+	if err != nil {
+		fmt.Println(err)
+		return
 	}
-	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "pokemon not found"})
+	fmt.Printf("Client value %v\n", client)
+
+	collection := client.Database("pokemon-book").Collection("pokemon")
+	fmt.Printf("Collection value %v\n", collection)
+
+	result := pokemon{}
+	err = collection.FindOne(context.Background(), bson.D{{"id", id}}).Decode(&result)
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "pokemon not found"})
+		// log.Fatal(err)
+		return
+	}
+	c.IndentedJSON(http.StatusOK, result)
 }
 
 func updatePokemonByID(c *gin.Context) {
@@ -121,29 +169,75 @@ func updatePokemonByID(c *gin.Context) {
 
 	if err := c.BindJSON(&newPokemon); err != nil {
 		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "object can't be parsed into JSON"})
+		return
 	}
 
-	for s, a := range pokemons {
-		if a.ID == newPokemon.ID {
-			pokemons[s] = newPokemon
-			c.IndentedJSON(http.StatusOK, gin.H{"message": "pokemon was updated"})
+	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://root:example@localhost:27017"))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("Client value %v\n", client)
+
+	collection := client.Database("pokemon-book").Collection("pokemon")
+	fmt.Printf("Collection value %v\n", collection)
+
+	opts := options.FindOneAndUpdate().SetUpsert(true)
+	filter := bson.D{{"id", newPokemon.ID}}
+	update := bson.D{{"$set", bson.D{
+		{"name", newPokemon.Name},
+		{"islegendary", newPokemon.IsLegendary},
+		{"color", newPokemon.Color},
+	}}}
+	var updatedPokemon bson.M
+	err = collection.FindOneAndUpdate(
+		context.Background(),
+		filter,
+		update,
+		opts,
+	).Decode(&updatedPokemon)
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			// pokemons = append(pokemons, newPokemon)
+			res, err := collection.InsertOne(context.Background(), newPokemon)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			id := res.InsertedID
+			fmt.Printf("id value %v\n", id)
+
+			c.IndentedJSON(http.StatusCreated, newPokemon)
 			return
 		}
+		log.Fatal(err)
 	}
-
-	pokemons = append(pokemons, newPokemon)
-	c.IndentedJSON(http.StatusCreated, newPokemon)
+	c.IndentedJSON(http.StatusOK, gin.H{"message": "pokemon was updated"})
 }
 
 func deletePokemonByID(c *gin.Context) {
 	id := c.Param("id")
 
-	for s, a := range pokemons {
-		if a.ID == id {
-			pokemons = append(pokemons[:s], pokemons[s+1:]...)
-			c.IndentedJSON(http.StatusOK, gin.H{"message": "pokemon was deleted"})
-			return
-		}
+	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://root:example@localhost:27017"))
+	if err != nil {
+		fmt.Println(err)
+		return
 	}
-	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "pokemon not found"})
+	fmt.Printf("Client value %v\n", client)
+
+	collection := client.Database("pokemon-book").Collection("pokemon")
+	fmt.Printf("Collection value %v\n", collection)
+
+	_, err = collection.DeleteOne(context.Background(), bson.D{{"id", id}})
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "pokemon not found"})
+		// log.Fatal(err)
+		return
+	}
+	// pokemons = append(pokemons[:s], pokemons[s+1:]...)
+	c.IndentedJSON(http.StatusOK, gin.H{"message": "pokemon was deleted"})
 }
